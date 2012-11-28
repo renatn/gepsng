500a49183c18d67f21132eb31bf478ec559be2d3
Renat Nasyrov <renatn@gmail.com>
2012-11-28 18:06:03 +0400

    geps20-1 Создать форму просмотра обращений

--- frontend/src/main/webapp/static/js/app.js
+++ frontend/src/main/webapp/static/js/app.js
@@ -7,13 +7,9 @@ var app = app || {};
 
         el: '#gepsapp',
 
-        events: {
-            'click #select-all': 'selectAll'
-        },
 
         initialize: function() {
             console.log('init main view');
-            this.allCheckbox = $('#select-all')[0];
             this.content = $('.geps-content')[0];
             app.Messages.on('reset', this.addAll, this );
             app.Messages.fetch();
@@ -27,16 +23,8 @@ var app = app || {};
         addAll: function() {
             console.log('add all');
             $(this.content).html((new app.MessageListView()).render().el);
-        },
-
-        selectAll: function() {
-            console.log("select all messages");
-            var selected = this.allCheckbox.checked;
-
-            app.Messages.each(function(message) {
-                message.setSelected(selected);
-            });
         }
+
     });
 
 
--- frontend/src/main/webapp/static/js/view/message_list.js
+++ frontend/src/main/webapp/static/js/view/message_list.js
@@ -9,6 +9,10 @@ var app = app || {};
 
         tagName: 'div',
 
+        events: {
+            'click #select-all': 'selectAll'
+        },
+
         template: _.template($('#messages-table-template').html()),
 
         initialize: function() {
@@ -18,13 +22,21 @@ var app = app || {};
         render: function() {
             console.log('render list view');
             this.$el.html(this.template({}));
-            app.Messages.each(this.addOne, this);
+            app.Messages.each(this.addMessage, this);
             return this;
         },
 
-        addOne: function(message) {
+        addMessage: function(message) {
             console.log('add one');
             $('tbody', this.el).append((new app.MessageRowView({model: message})).render().el);
+        },
+
+        selectAll: function() {
+            var selected = this.$el.find('#select-all')[0].checked;
+
+            app.Messages.each(function(message) {
+                message.setSelected(selected);
+            });
         }
 
     });

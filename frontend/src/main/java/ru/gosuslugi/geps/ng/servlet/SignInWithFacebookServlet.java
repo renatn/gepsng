eaa7c17225bb97b0ec57abff568bee6367c914dd
Renat Nasyrov <renatn@gmail.com>
2012-12-29 16:32:09 +0400

    worked fb authentication

--- frontend/src/main/java/ru/gosuslugi/geps/ng/servlet/SignInWithFacebookServlet.java
+++ frontend/src/main/java/ru/gosuslugi/geps/ng/servlet/SignInWithFacebookServlet.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -28,6 +29,8 @@ import java.util.Collections;
 public class SignInWithFacebookServlet extends HttpServlet {
 
     private final static String SIGNIN_URL = "http://gepsapp.renatn.com:8080/geps/signin";
+    private final static String CLIENT_ID = "505845966105387";
+    private final static String SECRET = "e6a4d500347f4238650af16c846e1dba"; // TODO: move to properties
 
     private final static Logger logger = Logger.getLogger(SignInWithFacebookServlet.class);
 
@@ -40,60 +43,54 @@ public class SignInWithFacebookServlet extends HttpServlet {
             return;
         }
 
+        // Request Facebook User Profile
         String token = requestAccessToken(code);
+        if (token.startsWith("{")) {
+            throw new IOException("error on requesting token: " + token + " with code: " + code);
+        }
         String graph = requestUserData(token);
         logger.info("Graph: " + graph);
 
+        // Create authentication
         GrantedAuthority authority = new SimpleGrantedAuthority("ROLE_USER");
         Collection<GrantedAuthority> authorities = Collections.singletonList(authority);
-
-        Authentication authentication = new UsernamePasswordAuthenticationToken("Renat Nasyrov", "qwerty", authorities);
+        Authentication authentication = new UsernamePasswordAuthenticationToken("Renat Nasyrov", graph, authorities);
         SecurityContextHolder.getContext().setAuthentication(authentication);
+        req.getSession().setAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY, SecurityContextHolder.getContext());
 
-        logger.info("CREATE AUTHENTICATION");
-
-        resp.sendRedirect("/geps");
+        // Redirect to main page
+        resp.sendRedirect("/geps/#!/");
     }
 
-    private String requestUserData(String token) {
-        String graph = null;
-        try {
-            String g = "https://graph.facebook.com/me?" + token;
-            URL u = new URL(g);
-            URLConnection c = u.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-            String inputLine;
-            StringBuilder b = new StringBuilder();
-            while ((inputLine = in.readLine()) != null)
-                b.append(inputLine).append("\n");
-            in.close();
-            graph = b.toString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return graph;
+    private String requestUserData(String token) throws IOException {
+        String g = "https://graph.facebook.com/me?" + token;
+        URL u = new URL(g);
+        URLConnection c = u.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+        String inputLine;
+        StringBuilder b = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            b.append(inputLine).append("\n");
+        in.close();
+        return b.toString();
     }
 
-    private String requestAccessToken(String code) {
-        String token = null;
-        try {
-            String g = "https://graph.facebook.com/oauth/access_token?client_id=505845966105387&redirect_uri="
-                    + URLEncoder.encode(SIGNIN_URL, "UTF-8")
-                    + "&client_secret=e6a4d500347f4238650af16c846e1dba&code=" + code;
-            URL u = new URL(g);
-            URLConnection c = u.openConnection();
-            BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
-            String inputLine;
-            StringBuilder b = new StringBuilder();
-            while ((inputLine = in.readLine()) != null)
-                b.append(inputLine).append("\n");
-            in.close();
-            token = b.toString();
-            if (token.startsWith("{"))
-                throw new Exception("error on requesting token: " + token + " with code: " + code);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return token;
+    private String requestAccessToken(String code) throws IOException {
+        String g = String.format(
+                "https://graph.facebook.com/oauth/access_token?client_id=%s&redirect_uri=%s&client_secret=%s&code=%s",
+                CLIENT_ID,
+                URLEncoder.encode(SIGNIN_URL, "UTF-8"),
+                SECRET,
+                code
+        );
+        URL u = new URL(g);
+        URLConnection c = u.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(c.getInputStream()));
+        String inputLine;
+        StringBuilder b = new StringBuilder();
+        while ((inputLine = in.readLine()) != null)
+            b.append(inputLine).append("\n");
+        in.close();
+        return b.toString();
     }
 }
